c4da666ec7eb620c106d7433bfc9f27d340df2cb
Fatih Turker <fthturker80@gmail.com>
2023-03-07 23:00:18 +0300

    07.03.2023

---
+++ day3/rangefunction.py
@@ -0,0 +1,16 @@
+# range() : values between the range
+
+# list() belirli aralık içindeki değerleri alacak başka bir işlevdir
+print(range(10)) # range(0, 10)
+print(list(range(10)))  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list(range(5,10))) # [5, 6, 7, 8, 9]
+
+# print only add numbers between 1-10
+print(list(range(1,10,2))) # [1, 3, 5, 7, 9]
+
+# print only even numbers between 1-10
+print(list(range(0,10,2))) # [0, 2, 4, 6, 8]
+
+print(list(range(10,1,-1))) # [10, 9, 8, 7, 6, 5, 4, 3, 2]
+
+print(list(range(-10,-5))) # [-10, -9, -8, -7, -6]
---
+++ day3/whileloopdemo.py
@@ -0,0 +1,9 @@
+# while loop
+
+# print 1.....10 numbers
+i = 1  # initilization
+
+while i <= 10:  # condition
+    print(i)
+    i = i + 1  # incrementation
+print("Done!!!!")
