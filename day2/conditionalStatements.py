ac146bceb684b9ee73e38689a1a8e95ca517b4e0
Fatih Turker <fthturker80@gmail.com>
2023-03-07 21:51:22 +0300

    07.03.2023

--- day2/conditionalStatements.py
+++ day2/conditionalStatements.py
@@ -40,10 +40,49 @@
 #     print("not one")
 
 #Example 4  : Find a number is even/add   num%2=0
-num=10
+# num=10
+#
+# if num%2==0:
+#     print("Given number is even")
+# else:
+#     print("Given is number add")
 
-if num%2==0:
-    print("Given number is even")
+# Example 5 : if else in single line (ternary operator)
+# num=10
+# print('Even number') if num%2==0 else print ('odd number')
+
+# Example 6 : if else - Multiple statements in Single Line
+# çoklu print deyimlerini {} parantezler içinde olur ve ardındam koşulu sürdürmek if else
+
+# a=20
+# {print('hello'),print('python')} if a>=10 else {print('hi'),print('java')}
+
+# Example 7 : Multiple conditions using elif
+
+weekno = 7
+
+if weekno == 1:
+    print('sunday')
+elif weekno == 2:
+    print('monday')
+elif weekno == 3:
+    print('tuesday')
+elif weekno == 4:
+    print('wednesday')
+elif weekno == 5:
+    print('thurday')
+elif weekno == 6:
+    print('friday')
+elif weekno == 7:
+    print('saturday')
 else:
-    print("Given is number add")
+    print('Invalid week number')
 
+"""
+Assigments
+----------------
+1) Check the given number is Positive or negative
+2) Check the largest of 2 numbers
+3) Check the largest of 3 numbers
+4) Print week number if you provide weekname as input
+"""
